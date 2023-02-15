2e2c3b40c4086f4dd392d8efd5f64cda4e4299ab
Bhawana <bhawanasaxena1502@gmail.com>
2023-02-15 13:05:19 +0530

    added new func muleven

--- multiply.py
+++ multiply.py
@@ -2,3 +2,12 @@ def mul(a,b,c):
     return a*b*c
 
 print(mul(3,6,4))
+
+
+def muleven(*a):
+    n=1
+    for i in a:
+        if i%2==0:
+            n =n*i
+    return n
+print(muleven(1,2,3,4,5,6,7,8))
